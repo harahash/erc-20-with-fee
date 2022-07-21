6d69762743365f4e2d09ee2fce3873572f3718c7
harahash <alexanderkharakhash@gmail.com>
2022-07-21 14:07:23 +0300

    contract changes

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,109 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-library SafeMath {
-    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            uint256 c = a + b;
-            if (c < a) return (false, 0);
-            return (true, c);
-        }
-    }
-
-    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (b > a) return (false, 0);
-            return (true, a - b);
-        }
-    }
-
-    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (a == 0) return (true, 0);
-            uint256 c = a * b;
-            if (c / a != b) return (false, 0);
-            return (true, c);
-        }
-    }
-
-    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (b == 0) return (false, 0);
-            return (true, a / b);
-        }
-    }
-
-    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (b == 0) return (false, 0);
-            return (true, a % b);
-        }
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a + b;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a - b;
-    }
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a * b;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a / b;
-    }
-
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a % b;
-    }
-
-    function sub(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
-        unchecked {
-            require(b <= a, errorMessage);
-            return a - b;
-        }
-    }
-
-    function div(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
-        unchecked {
-            require(b > 0, errorMessage);
-            return a / b;
-        }
-    }
-
-    function mod(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
-        unchecked {
-            require(b > 0, errorMessage);
-            return a % b;
-        }
-    }
-}
-
 contract MyToken {
-    using SafeMath for uint256;
-
     address public manager;
     string public constant name = "MyToken";
     string public constant symbol = "TKN";
     uint8 public constant decimals = 18;
 
     uint256 public totalSupply;
-    uint256 public totalSpending;
+    uint256 public fee = 5;
+    // uint256 public totalSpending;
 
     mapping (address => uint256) balances;
     mapping (address => mapping(address => uint256)) allowed;
@@ -115,33 +21,49 @@ contract MyToken {
         manager = msg.sender;
     }
 
-    function mint(address to, uint256 value) public {
-        balances[to] = balances[to].add(value);
-        totalSupply = totalSupply.add(value);
+    modifier onlyOwner {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function mint(address to, uint256 value) public onlyOwner {
+        balances[to] = balances[to] + value;
+        totalSupply = totalSupply + value;
     }
 
     function transfer(address _to, uint256 _value) public {
-        totalSpending = _value + _value.mul(5).div(100);
+        uint256 transactionFee = feeCalculation(_value);
+        uint256 totalSpending = _value + transactionFee;
         require(balances[msg.sender] >= totalSpending);
-        // require(balances[msg.sender] >= _value);
-        balances[msg.sender] = balances[msg.sender].sub(totalSpending);
-        balances[_to] = balances[_to].add(_value);
-        balances[manager] = balances[manager].add(_value.mul(5).div(100)); // 5% fee
+
+        balances[msg.sender] = balances[msg.sender] - totalSpending;
+        balances[_to] = balances[_to] + _value;
+        sendingFeeToOwner(transactionFee); // 5% fee
 
         emit Transfer(msg.sender, _to, _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public {
-        totalSpending = _value + _value.mul(5).div(100);
+        uint256 transactionFee = feeCalculation(_value);
+        uint256 totalSpending = _value + transactionFee;
         require(balances[_from] >= totalSpending &&
                 allowed[_from][msg.sender] >= _value
                 );
-        balances[_from] = balances[_from].sub(totalSpending) ;
-        balances[_to] = balances[_to].add(_value);
-        balances[manager] = balances[manager].add(_value.mul(5).div(100)); // 5% fee
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+
+        balances[_from] = balances[_from] - totalSpending;
+        balances[_to] = balances[_to] + _value;
+        sendingFeeToOwner(transactionFee); // 5% fee
+        allowed[_from][msg.sender] = allowed[_from][msg.sender] - _value;
 
         emit Transfer(_from, _to, _value);
+    }
+
+    function feeCalculation(uint256 _value) public view returns(uint256) {
+        return _value * fee / 100;
+    }
+
+    function sendingFeeToOwner(uint256 _fee) public {
+        balances[manager] = balances[manager] + _fee;
     }
 
     function approve(address _spender, uint256 _value) public {
